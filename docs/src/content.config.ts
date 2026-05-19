3b6ce8c5fb3a0044298661542ecfcdc643217fd7
Sergei Shmakov <shmakov@skbkontur.ru>
2026-05-19 13:31:15 +0500

    chore: google verify + package.lock

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,11 +1,11 @@
 import { defineCollection } from "astro:content";
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
-import { docsAndBlogSchema } from "starlight-blog/schema";
+import { blogSchema } from "starlight-blog/schema";
 
 export const collections = {
 	docs: defineCollection({
 		loader: docsLoader(),
-		schema: docsSchema({ extend: docsAndBlogSchema }),
+		schema: docsSchema({ extend: blogSchema }),
 	}),
 };
